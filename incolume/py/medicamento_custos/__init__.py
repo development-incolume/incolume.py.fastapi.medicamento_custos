663e861bc375b1eea9ca43dbc727f44b8fcf87d7
Britodfbr <britodfbr@gmail.com>
2023-02-21 23:51:17 -0300

    feat: Unificado versionamento do projeto

--- incolume/py/medicamento_custos/__init__.py
+++ incolume/py/medicamento_custos/__init__.py
@@ -1,1 +1,17 @@
-__author__ = "@britodfbr"  # pragma: no cover
+"""
+Principal Module.
+
+Update metadata from version by semver
+"""
+from tomllib import load
+from pathlib import Path
+
+configfile = Path(__file__).parents[3].joinpath("pyproject.toml")
+versionfile = Path(__file__).parent.joinpath("version.txt")
+
+with configfile.open('rb') as file:
+    versionfile.write_text(f"{load(file)['tool']['poetry']['version']}\n")
+__version__ = versionfile.read_text().strip()
+
+if __name__ == '__main__':
+    print(__version__)
